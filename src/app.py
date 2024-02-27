9f62a5fb2236d2bd6098bd7039eba762a5ecc675
miyago9267 <miyagomizugen@gmail.com>
2024-02-27 14:51:43 +0800

    refector: move config into models

--- src/app.py
+++ src/app.py
@@ -1,21 +1,12 @@
 import discord
-import asyncio, time, random, os, re
-
+import os
 from models.bot import Bot
 from dotenv import load_dotenv
 
 load_dotenv()
 
-intents = discord.Intents.default()
-intents.message_content = True
-bot = Bot(command_prefix=os.getenv('COMMAND_PREFIX'), intents=intents)
-
-@bot.event
-async def on_ready():
-    # bot.add_cog(models.BOT_NAME(bot))
-    pass
-
 def bot_start():
+    bot = Bot()
     bot.setup(os.getenv('BOT_TOKEN'))
 
 if __name__=='__main__':
