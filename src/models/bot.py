bf25408df5b8a24f95cbe07f0be121c197439996
miyago9267 <miyagomizugen@gmail.com>
2024-02-27 14:52:37 +0800

    refector: init super class inside bot

--- src/models/bot.py
+++ src/models/bot.py
@@ -1,17 +1,24 @@
-from dotenv import load_dotenv
 import discord.ext.commands as commands
+import discord
+import os
+from dotenv import load_dotenv
 
 load_dotenv()
 
 class Bot(commands.Bot):
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+        intents = discord.Intents.all()
+        super().__init__(
+            command_prefix=os.getenv('COMMAND_PREFIX'),
+            intents=intents
+        )
 
-    def setup(self, token):
-        self.run(token)
+    def setup_hook(self, token):
+        self.start(token)
 
     async def on_ready(self):
         await self.add_cog(BotEventsCog(self))
+        # await self.add_cog(BotCommandsCog(self))
 
 class BotEventsCog(commands.Cog):
     def __init__(self, bot):
